5e0355d32b9b5fd69e09880974b423410e74929c
Hypackel <contact@hypackel.com>
2024-02-28 22:27:37 -0500

    eee

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 5001
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
